d1543e3ccf30ca5cc78b08d166f894686ff96ca5
anuradha-bhandari <131469707+anuradha-bhandari@users.noreply.github.com>
2025-08-03 15:40:53 +0530

    register save

--- src/controller/regctrl.js
+++ src/controller/regctrl.js
@@ -1,4 +1,15 @@
+let model=require("../model/model.js");
+
 exports.register=(req,res)=>{
+
   let {name,email,password,address,role}=req.body;
-  console.log(name,email,password,address,role);
+  let result=model.registerUser(name,email,password,address,role);
+  result.then((r)=>{
+    if(r. affectedRows>0){
+          res.send("Data added successfully");
+    }else{
+      res.send("Data not added");
+    }
+  })
+  .catch(err=>console.log(err))
 }
